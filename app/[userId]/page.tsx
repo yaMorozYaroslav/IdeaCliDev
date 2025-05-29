59e504553e825e29601fb9e4321c4aaa5ff713b5
Yaroslav Moroz <ya.moroz.yaroslav@gmail.com>
2025-05-29 20:51:56 +0200

    05292051

--- app/[userId]/page.tsx
+++ app/[userId]/page.tsx
@@ -1,8 +1,8 @@
 // ✅ app/[userId]/page.tsx
 import ClientUserProfile from "./ClientUserProfile";
 import { cookies } from "next/headers";
-import getUser from "/lib/getUser";
-import getBaseUrl from "/lib/getBaseUrl";
+import getUser from "../../lib/getUser";
+import getBaseUrl from "../../lib/getBaseUrl";
 
 export default async function Page(props: { params: { userId: string } }) {
   const { userId } = await props.params;
