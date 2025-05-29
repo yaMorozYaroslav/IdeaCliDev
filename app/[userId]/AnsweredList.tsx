c4d5e40163dd70b79559111492eb5cfa80e3cb5f
Yaroslav Moroz <ya.moroz.yaroslav@gmail.com>
2025-05-29 21:34:16 +0200

    05292034

--- app/[userId]/AnsweredList.tsx
+++ app/[userId]/AnsweredList.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import styled from "styled-components";
+import getBaseUrl from "../../lib/getBaseUrl";
 
 const Spinner = styled.div`
   border: 4px solid #f3f3f3;
@@ -34,7 +37,7 @@ export default function AnsweredList({
     q?.authorId === user?.googleId || isOwner || user?.status === "admin";
 
   const handleDelete = async (questionId: string) => {
-    const res = await fetch(`/personal/${questionId}`, {
+    const res = await fetch(`${getBaseUrl()}/personal/${questionId}`, {
       method: "DELETE",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ userId: user?.googleId }),
