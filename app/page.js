51958c409d357459b5805bc39f0db517be5eab8a
Yaroslav Moroz <ya.moroz.yaroslav@gmail.com>
2025-08-14 19:48:21 +0200

    2508141948

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 // app/page.js (Server Component)
 import HomeClient from "/comps/HomeClient";
-import { getUserFromCookiesServer } from "../lib/getUserFromCookiesServer"; // ✅ server-safe cookie reader
+import { getUserFromCookiesServer } from "@/lib/cookies/getUserFromCookiesServer"; // ✅ server-safe cookie reader
 
 export default async function Home() {
   const user = getUserFromCookiesServer(); // ✅ reads user_data from cookies safely
